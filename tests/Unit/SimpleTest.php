22cbbb00e3ab767e41b8e371b4c373f364607a4e
kirillbdev <kirill.babinec@gmail.com>
2021-02-03 13:42:40 +0200

    Simple test + travis.ci basic

---
+++ tests/Unit/SimpleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace kirillbdev\MediaManager\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class SimpleTest extends TestCase
+{
+    public function testEquals()
+    {
+        $this->assertEquals(0, 0);
+    }
+}
